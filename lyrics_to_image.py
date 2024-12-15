3d73efc6f7e8a421a0cdd3cd281357b39212f0a5
Sam Hamamji <samhamamji0@gmail.com>
2024-12-15 17:02:40 +0200

    Extract script logic to functions

--- lyrics_to_image.py
+++ lyrics_to_image.py
@@ -17,38 +17,59 @@ parser.add_argument("--magical_atmosphere", action="store_true")
 parser.add_argument("--include_intricate_details", action="store_true")
 
 
+def generate_image_from_lyrics(
+    song_lyrics: str,
+    log_description=False,
+    log_url=False,
+    image_size="1024x1024",
+    max_retries=3,
+    magical_atmosphere=False,
+    include_intricate_details=False,
+    artist: str | None = None,
+):
+    prompt = prompts.get_scene_description_prompt(
+        song_lyrics, magical_atmosphere, include_intricate_details, artist
+    )
+
+    for i in range(max_retries):
+        try:
+            scene_description = generation.generate_text(prompt)
+            if log_description:
+                print(f"Scene description: {scene_description}", end="\n\n")
+            image = generation.generate_image(
+                f"{scene_description}\n\nDo not include words in the generated image.",
+                image_size,
+                log_url,
+            )
+            break
+        except openai.BadRequestError as error:
+            if i == max_retries - 1:
+                print("Image generation rejected. Max number of retries exceeded!")
+                raise error
+
+            print("Image generation rejected for content policy violation. Retrying...")
+            prompt = prompts.get_scene_fixing_prompt(scene_description)
+
+    return image
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.lyrics, "r", encoding="utf-8") as file:
         song_lyrics = file.read()
 
-    prompt = prompts.get_scene_description_prompt(
+    output = generate_image_from_lyrics(
         song_lyrics,
+        args.log_description,
+        args.log_url,
+        args.image_size,
+        args.max_retries,
         args.magical_atmosphere,
         args.include_intricate_details,
         args.artist,
     )
 
-    for i in range(args.max_retries):
-        try:
-            scene_description = generation.generate_text(prompt)
-            if args.log_description:
-                print(f"Scene description: {scene_description}", end="\n\n")
-            image = generation.generate_image(
-                f"{scene_description}\n\nDo not include words in the generated image.",
-                args.image_size,
-                args.log_url,
-            )
-            break
-        except openai.BadRequestError as e:
-            if i == args.max_retries - 1:
-                print("Image generation rejected. Max number of retries exceeded!")
-                exit(1)
-
-            print("Image generation rejected for content policy violation. Retrying...")
-            prompt = prompts.get_scene_fixing_prompt(scene_description)
-
     with open(args.output, "wb") as file:
-        file.write(image)
+        file.write(output)
     print(f"Image saved successfully at {args.output}")
--- lyrics_to_video.py
+++ lyrics_to_video.py
@@ -12,20 +12,24 @@ parser.add_argument("--log_description", action="store_true")
 parser.add_argument("--log_url", action="store_true")
 
 
+def generate_video_from_lyrics(lyrics: str, log_description=False, log_url=False):
+    prompt = prompts.get_video_description_prompt(lyrics)
+
+    frames_description = generation.generate_text(prompt)
+    if log_description:
+        print(f"Scene description: {frames_description}", end="\n\n")
+    scene_frames: dict[str, str] = json.loads(frames_description)
+
+    return generation.generate_video(scene_frames, log_url)
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.lyrics, "r", encoding="utf-8") as file:
-        song_lyrics = file.read()
+        lyrics = file.read()
 
-    prompt = prompts.get_video_generation_prompt(song_lyrics)
-
-    frames_description = generation.generate_text(prompt)
-    if args.log_description:
-        print(f"Scene description: {frames_description}", end="\n\n")
-    scene_frames: dict[str, str] = json.loads(frames_description)
-
-    video_bytes = generation.generate_video(scene_frames, args.log_url)
+    video_bytes = generate_video_from_lyrics(lyrics, args.log_description, args.log_url)
 
     with open(args.output, "wb") as file:
         file.write(video_bytes)
