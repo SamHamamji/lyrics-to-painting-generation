ad07d9bfa718b65e36b0d13943974f1c147683e6
Sam Hamamji <samhamamji0@gmail.com>
2024-12-15 15:54:15 +0200

    Add setup datasets script

---
+++ setup_datasets.sh
@@ -0,0 +1,4 @@
+kaggle datasets download ikarus777/best-artworks-of-all-time
+kaggle datasets download neisse/scrapped-lyrics-from-6-genres
+unzip -qu best-artworks-of-all-time.zip
+unzip -qu scrapped-lyrics-from-6-genres
